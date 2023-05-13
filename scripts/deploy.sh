b1bd58bd769162f880c6f32b5b2f1c2b39d9cd45
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-13 09:06:27 +0900

    commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,7 +26,7 @@ DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/action/admin/deploy.log
 #nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/admin/deploy_err.log &
 
-nohup java -Dspring.profiles.active=prod -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/admin/deploy_err.log &
+nohup java -Dspring.profiles.active=prod -jar $DEPLOY_JAR
 
 
 
